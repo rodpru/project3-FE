ef05e2be01f9865c555ccae3c61c920c4ac586f5
djnokin <nokindj@gmail.com>
2020-12-04 13:41:06 +0000

    miguel

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,7 +5,7 @@ class Homepage extends React.Component {
   render() {
     return (
       <div>
-        <div>
+        <div className="container-fluid">
           <div
             id="carouselExampleSlidesOnly"
             className="carousel slide"
@@ -14,13 +14,14 @@ class Homepage extends React.Component {
             <div className="carousel-inner">
               <div className="carousel-item active">
                 <img
-                  src="/images/hisu-lee-in9QlspOG6w-unsplash.jpg"
+                  src="/images/kelly-sikkema-JRVxgAkzIsM-unsplash.jpg"
                   className="d-block w-100"
                   alt="schoolpic"
                 />
               </div>
               <div className="carousel-item">
                 <img
+                  id="img2"
                   src="/images/pro-church-media-2DTE3ePfnD8-unsplash.jpg"
                   className="d-block w-100"
                   alt="schoolpic"
@@ -28,23 +29,26 @@ class Homepage extends React.Component {
               </div>
             </div>
           </div>
-          <Link to={"/nurseries"}>
-            <button className="btn btn-light">Nurseries</button>
-          </Link>
-          <Link to={"/kindergartens"}>
-            <button className="btn btn-light">Kindergartens</button>
-          </Link>
-          {/* <div className="img-text">
-          <h2>
-            Find the best kindergarten <br />
-            or nursery for your children!
-          </h2>
-          <p>
-            Lorem Ipsum has been the industry's standard dummy
-            <br /> text ever since the 1500s, when an unknown printer took a
-            <br /> galley of type and scrambled it to make a type specimen book.
-          </p>
-        </div> */}
+          <div id="img-text" className="container-xl img-text">
+            <h1>
+              Find the best kindergarten <br />
+              or nursery for your children!
+            </h1>
+            <p>
+              Lorem Ipsum has been the industry's standard dummy
+              <br /> text ever since the 1500s, when an unknown printer took a
+              <br /> galley of type and scrambled it to make a type specimen
+              book.
+            </p>
+            <Link to={"/nurseries"}>
+              <button className="btn btn-light">Nurseries</button>
+            </Link>
+            <Link to={"/kindergartens"}>
+              <button className="btn btn-light">Kindergartens</button>
+            </Link>
+          </div>
+          <h2>Best Kindergartens</h2>
+          <p>Find the best kindergartens in Lisbon</p>
         </div>
       </div>
     );
