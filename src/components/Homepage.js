1c01ce2ce69ae92e5773ca201e7bfab76f6894cd
djnokin <nokindj@gmail.com>
2020-12-04 16:13:22 +0000

    miguel

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import KindergartensApi from "../utils/api-cml";
 
 class Homepage extends React.Component {
+  state = {
+    kindergartens: [],
+  };
+
+  componentDidMount() {
+    const kindergartens = new KindergartensApi();
+    kindergartens.getAllKindergartens().then((response) => {
+      return this.setState({
+        kindergartens: response.data.features,
+      });
+    });
+  }
+
+  getRandom() {
+    // filter the kindergarten array
+    let copy = [...this.state.kindergartens]; // all the array
+    let result = [];
+    for (let i = 0; i < 4; i++) {
+      let randomNumber = [Math.floor(Math.random() * copy.length)];
+      result.push(copy[randomNumber]);
+      copy.splice(randomNumber, 1);
+    }
+    this.setState({
+      kindergartens: result,
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.kindergartens.length > 4) {
+      this.getRandom();
+    }
+  }
+
   render() {
+    // function getRandomKindergarten(items) {
+    //   return [Math.floor(Math.random() * items.length)];
+    // }
+
     return (
       <div>
         <div id="home-container">
@@ -54,6 +92,31 @@ class Homepage extends React.Component {
           <div id="kinder-text">
             <h2>Best Kindergartens</h2>
             <p>Find the best kindergartens in Lisbon</p>
+            <div className="card-deck d-flex flex-wrap">
+              {this.state.kindergartens.map((kindergarten, index) => {
+                return (
+                  <div
+                    className="card col-sm-3"
+                    key={index}
+                    style={{ width: "200px" }}
+                  >
+                    <img
+                      src="/images/thiago-cerqueira-Wr3HGvx_RSM-unsplash.jpg"
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        {kindergarten.attributes.INF_NOME}
+                      </h5>
+                      <p className="card-text">
+                        {kindergarten.attributes.INF_DESCRICAO}
+                      </p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
